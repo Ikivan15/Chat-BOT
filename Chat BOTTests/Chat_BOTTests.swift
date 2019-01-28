2dbf3da3955c9a3b7cea75a2a30ff5580b9d0782
Коробов Иван <i.korobov@mbp046.local>
2019-01-28 15:57:31 +0300

    Initial Commit

---
+++ Chat BOTTests/Chat_BOTTests.swift
@@ -0,0 +1,34 @@
+//
+//  Chat_BOTTests.swift
+//  Chat BOTTests
+//
+//  Created by Коробов Иван on 28/01/2019.
+//  Copyright © 2019 Коробов Иван. All rights reserved.
+//
+
+import XCTest
+@testable import Chat_BOT
+
+class Chat_BOTTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
